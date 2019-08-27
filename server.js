cf479c8901f12c5d0b9c46ab6cf25816ed01b0e1
Pankaj Patel <p.patel@holidaypirates.com>
2019-08-27 21:45:51 +0200

    init server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: "application/*+json" }));
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: "text/html" }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", async (req, res) => res.send("Hello World"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
